7cab0cd16ba8b23ae8abc2615e68db9d2d6c6633
Evgeniy Talko <etalko@gmail.com>
2021-04-01 07:08:29 +0300

    add: replace fetch with swr

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,32 +1,27 @@
 import {useState} from "react";
 import {useEffect} from "react";
+import useSWR from 'swr';
 
 function SalesPage() {
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
   const [sales, setSales] = useState([]);
+  const {data, error} = useSWR("https://nextjs-test-c75bc-default-rtdb.europe-west1.firebasedatabase.app/sales.json");
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch("https://nextjs-test-c75bc-default-rtdb.europe-west1.firebasedatabase.app/sales.json")
-      .then(response => response.json())
-      .then(data => {
-        const transformedData = [];
-        for (let key in data) {
-          transformedData.push({
-            id: key,
-            username: data[key].name,
-            volume: data[key].volume,
-          })
-        }
-        setIsLoading(false);
-        setSales(transformedData);
-        console.log(transformedData);
+    const transformedData = [];
+    for (let key in data) {
+      transformedData.push({
+        id: key,
+        username: data[key].name,
+        volume: data[key].volume,
       })
-  }, [])
+    }
+    setSales(transformedData);
+  }, [data])
 
-  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
-  if (!sales) return <p>Product not set</p>;
+  if (!data || !sales) return <p>Loading...</p>;
 
   return (
     <>
